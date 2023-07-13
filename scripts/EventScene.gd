0b74e9462483ee05829967723c55f102aecf886d
Nick Müller <nick.mueller@uni-greifswald.de>
2023-07-13 18:59:31 +0200

    fixing layout issues; scrollbar

--- scripts/EventScene.gd
+++ scripts/EventScene.gd
@@ -3,7 +3,7 @@ extends Control
 var event:Event
 
 func _ready():
-	$title.text = event.title
-	$description.text = event.description
-	$costs.text = str(event.costs, "/DAY")
-	$duration.text = str(event.duration, " DAYS")
+	find_node("title").text = event.title
+	find_node("description").text = event.description
+	find_node("costs").text = str(event.costs, "/DAY")
+	find_node("duration").text = str(event.duration, " DAYS")
