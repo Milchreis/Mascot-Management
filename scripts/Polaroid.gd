c9c5dcbbf545a04d2ea8b1b5ae458362bcb49549
Nick Müller <nick.mueller@uni-greifswald.de>
2023-07-17 19:51:33 +0200

    Colors and animations

--- scripts/Polaroid.gd
+++ scripts/Polaroid.gd
@@ -9,6 +9,8 @@ export(int) var salaryPerDay = 10
 
 var mascot:Mascot
 
+var COOLDOWN_MAX := 20
+var selectCoolDown := COOLDOWN_MAX
 var dotEmpty = load("res://gfx/dot_empty.png")
 var dotFilled = load("res://gfx/dot_filled.png")
 
@@ -16,11 +18,12 @@ func _ready():
 	$pic/Sprite.texture = load(mascot.spriteImage)
 	$salary.text = str(mascot.salaryPerDay) + "$/d"
 	$name.text = mascot.nickname.to_upper()
-	$pic.color = mascot.bgColor
-	$pic/AnimationPlayer.play("idle")
-	
-	if showStats: $hoverBg.rect_size.y = 105
-	else: $hoverBg.rect_size.y = rect_min_size.y
+		
+	if showStats: 
+		$hoverBg.rect_size.y = 105
+	else: 
+		$hoverBg.rect_size.y = 76
+		rect_min_size.y = 76
 	
 	createStats($StatsPanel/improPosition, mascot.improvisation)
 	createStats($StatsPanel/reliabPosition, mascot.reliable)
@@ -32,7 +35,7 @@ func _process(_delta):
 	$Busy.visible = mascot.in_training or mascot.isInEvent()
 	$DaysRemaining.visible = mascot.in_training or mascot.isInEvent()
 	$DaysRemaining.text = str(mascot.getRemainingDays())
-	$pic/Sprite.flip_h = get_global_mouse_position().x > $pic/Sprite.global_position.x
+	$pic/Sprite.flip_h = get_global_mouse_position().x > $pic/Sprite.global_position.x - $pic/Sprite.texture.get_width()/2
 	# todo
 	$Ill.visible = false
 	
@@ -43,19 +46,27 @@ func _process(_delta):
 		setStats($StatsPanel/improPosition, mascot.improvisation)
 		setStats($StatsPanel/reliabPosition, mascot.reliable)
 		setStats($StatsPanel/charismaPosition, mascot.charisma)
+		
+	selectCoolDown = min(selectCoolDown + 1, COOLDOWN_MAX)
 
 func _input(event):
+	
 	if event is InputEventMouseButton and isMouseOver(event.position):
-		if event.button_index == BUTTON_LEFT:
-			print(get_parent().get_global_rect())
+		$pic/AnimationPlayer.play("hover")
+		
+		if event.button_index == BUTTON_LEFT and selectCoolDown == COOLDOWN_MAX:
+			selectCoolDown = 0
 			emit_signal("select", mascot)
+	
+	else: $pic/AnimationPlayer.stop()
 
 func isMouseOver(_position):
 	# it's a little hacky, but the InputEventMouseButton doesn't work with scroll together
 	# and the Polroid-scene as Button doesn't fire the signal on click.
-	var scrollAreaBeginY = 43
+	var scrollAreaBeginY = 40	
 	return (get_global_rect().has_point(_position) 
 		and _position.y > scrollAreaBeginY)
+		
 
 func createStats(positionNode, propertyValue):
 	for n in range(0, 5):
