4abc2a14bbc7f143146bcf78b17de03d7f47e7a1
Nick Müller <nick.mueller@uni-greifswald.de>
2023-08-03 15:53:09 +0200

    gameover: animated button

--- scripts/Gameover.gd
+++ scripts/Gameover.gd
@@ -12,6 +12,9 @@ func showScreen(model:GameModel):
 	SlideUtil.slideInFromBottom(self, self, 0.5)
 
 func _onTryAgainPressed():
+	SlideUtil.jumpControl(self, $Controls/TryAgain).connect("finished", self, "_slideOut")
+
+func _slideOut():
 	$Sprite/AnimationPlayer.stop()
 	SlideUtil.slideOutToBottom(self, self, 0.5).connect("finished", self, "_hide")
 	emit_signal("try_again")
