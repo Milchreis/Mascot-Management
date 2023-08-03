bd107e3828aae2f0aa3d2df1663790f9d94df40f
Nick Müller <nick.mueller@uni-greifswald.de>
2023-08-03 11:43:24 +0200

    Refactoring

--- scripts/Gameover.gd
+++ scripts/Gameover.gd
@@ -2,8 +2,19 @@ extends Node2D
 
 signal try_again
 
-func _ready():
+func showScreen(model:GameModel):
+	visible = true
+	$Controls/DaysTotal.text = str(model.passedDays)
+	$Controls/DaysBest.text = str(model.daysInFullSatisfactionBest)
+	
+	$Player.play()
 	$Sprite/AnimationPlayer.play("sad")
+	SlideUtil.slideInFromBottom(self, self, 0.5)
 
 func _onTryAgainPressed():
+	$Sprite/AnimationPlayer.stop()
+	SlideUtil.slideOutToBottom(self, self, 0.5).connect("finished", self, "_hide")
 	emit_signal("try_again")
+
+func _hide():
+	visible = false
