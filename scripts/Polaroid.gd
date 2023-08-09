4969e756f0a1af124019ed6c3617bcd1d8132808
Nick Müller <nick.mueller@uni-greifswald.de>
2023-08-09 20:57:00 +0200

    Refactoring; Hover for each skill

--- scripts/Polaroid.gd
+++ scripts/Polaroid.gd
@@ -9,15 +9,11 @@ var salaryPerDay = 10
 var clickable = true
 
 var mascot:Mascot
-
-var dotEmpty = load("res://gfx/dot_empty.png")
-var dotFilled = load("res://gfx/dot_filled.png")
-var dotHalfFilled = load("res://gfx/dot_half-filled.png")
 var sprites:Texture = load("res://gfx/mascots.png")
 
 func _ready():
 	$pic/Sprite.frame = mascot.spriteIndex
-	$salary.text = str(mascot.salaryPerDay) + "$/d"
+	$salary.text = "-" + str(mascot.salaryPerDay) + "$/d"
 	$name.text = mascot.nickname.to_upper()
 		
 	if showStats: 
@@ -26,9 +22,9 @@ func _ready():
 		$hoverBg.rect_size.y = 76
 		rect_min_size.y = 76
 	
-	createStats($StatsPanel/improPosition, mascot.improvisation)
-	createStats($StatsPanel/reliabPosition, mascot.reliable)
-	createStats($StatsPanel/charismaPosition, mascot.charisma)
+	$StatsPanel/Impro.value = mascot.improvisation
+	$StatsPanel/Charisma.value = mascot.charisma
+	$StatsPanel/Reliable.value = mascot.reliable
 
 func _process(_delta):
 	$StatsPanel.visible = showStats
@@ -48,9 +44,9 @@ func _process(_delta):
 		$hoverBg.visible = isMouseOver(get_global_mouse_position())
 	
 	if showStats:
-		setStats($StatsPanel/improPosition, mascot.improvisation)
-		setStats($StatsPanel/reliabPosition, mascot.reliable)
-		setStats($StatsPanel/charismaPosition, mascot.charisma)
+		$StatsPanel/Impro.value = mascot.improvisation
+		$StatsPanel/Charisma.value = mascot.charisma
+		$StatsPanel/Reliable.value = mascot.reliable
 	
 	if isMouseOver(get_global_mouse_position()):
 		$pic/AnimationPlayer.get_animation("hover").loop = true
@@ -58,7 +54,6 @@ func _process(_delta):
 	else: 
 		$pic/AnimationPlayer.get_animation("hover").loop = false
 		$pic/AnimationPlayer.queue("still")
-		
 
 func _input(event):
 	if event is InputEventMouseButton and isMouseOver(event.position):	
@@ -74,23 +69,3 @@ func isMouseOver(_position):
 	var scrollAreaBeginY = 40
 	return (get_global_rect().has_point(_position) 
 		and _position.y > scrollAreaBeginY)
-		
-func createStats(positionNode:Node2D, propertyValue:float):
-	for n in range(0, 5):
-		var stat = Sprite.new()
-		stat.texture = dotEmpty
-		
-		if propertyValue > float(n) + 0.49: stat.texture = dotHalfFilled
-		if int(propertyValue) > n: stat.texture = dotFilled
-		
-		stat.position = positionNode.position
-		stat.position.x += n * (stat.texture.get_width()+1)
-		positionNode.add_child(stat)
-		
-func setStats(positionNode, propertyValue):
-	for n in range(0, 5):
-		var stat:Sprite = positionNode.get_children()[n]
-		stat.texture = dotEmpty
-		
-		if propertyValue > float(n) + 0.49: stat.texture = dotHalfFilled
-		if int(propertyValue) > n: stat.texture = dotFilled
---
+++ scripts/SkillProperty.gd
@@ -0,0 +1,45 @@
+class_name SkillProperty
+extends Node2D
+
+export var shortLabel:String
+export var longLabel:String
+export var value:float
+
+var dotEmpty = load("res://gfx/dot_empty.png")
+var dotFilled = load("res://gfx/dot_filled.png")
+var dotHalfFilled = load("res://gfx/dot_half-filled.png")
+
+func _ready():
+	$label.text = shortLabel
+	createStats($position, value)
+
+func _process(_delta):
+	setStats($position, value)
+	
+func createStats(positionNode:Node2D, propertyValue:float):
+	for n in range(0, 5):
+		var stat = Sprite.new()
+		stat.texture = dotEmpty
+		
+		if propertyValue > float(n) + 0.49: stat.texture = dotHalfFilled
+		if int(propertyValue) > n: stat.texture = dotFilled
+		
+		stat.position = positionNode.position
+		stat.position.x += n * (stat.texture.get_width()+1)
+		positionNode.add_child(stat)
+		
+func setStats(positionNode, propertyValue):
+	for n in range(0, 5):
+		var stat:Sprite = positionNode.get_children()[n]
+		stat.texture = dotEmpty
+		
+		if propertyValue > float(n) + 0.49: stat.texture = dotHalfFilled
+		if int(propertyValue) > n: stat.texture = dotFilled
+
+func onEntered():
+	$position.visible = false
+	$label.text = longLabel
+
+func onExited():
+	$position.visible = true
+	$label.text = shortLabel
